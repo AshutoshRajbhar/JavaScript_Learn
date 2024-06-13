8758677c39de68f1bc68958baf16e16ddf400d37
Ashutosh Kumar Singh Rajbhar <121164888+AshutoshRajbhar@users.noreply.github.com>
2024-06-13 00:41:07 +0000

    data types added

---
+++ day_01/data_type.js
@@ -0,0 +1,21 @@
+// Data Types in javaScript
+
+// Primitive Data Type
+
+let name = "Ashutosh" // String Data Type
+let age = 29 // number // bigint
+// boolean => ture/false
+// null => standalone value/ representation of empty value
+// undefined => value not defined
+// symbol => unique component
+
+
+// Object Data Type
+
+console.log(typeof null)
+
+// o/p => object
+console.log(typeof undefined)
+
+// O/P => undefined
+
--- day_01/variable.js
+++ day_01/variable.js
@@ -1,10 +1,16 @@
 const accountId = 14553;
 let accountEmail = "ashutoshrajbhar@gmail.com";
-var  accountpassword = "12345";
+var accountpassword = "12345";
 accountCity = "Bareilly";
+let accountState;
 
 accountEmail = "aksr@gmail.com";
 accountpassword = "12121";
 accountCity = "Gurugram";
 
-console.table([accountEmail,accountPassword,accountCity]);
+console.table([accountEmail, accountpassword, accountCity, accountState]);
+
+/*
+Prefer not to use 'var'
+Beacuse of issue in block scope and functional scope
+*/
