881ff325d95769b84e2ac6d685570a8417e0df7b
Ashutosh Kumar Singh Rajbhar <121164888+AshutoshRajbhar@users.noreply.github.com>
2024-06-13 00:08:56 +0000

    variables

---
+++ day_01/variable.js
@@ -0,0 +1,10 @@
+const accountId = 14553;
+let accountEmail = "ashutoshrajbhar@gmail.com";
+var  accountpassword = "12345";
+accountCity = "Bareilly";
+
+accountEmail = "aksr@gmail.com";
+accountpassword = "12121";
+accountCity = "Gurugram";
+
+console.table([accountEmail,accountPassword,accountCity]);
