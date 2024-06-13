d776d6173cb38da4228674baeb50d12698317d19
Ashutosh Kumar Singh Rajbhar <121164888+AshutoshRajbhar@users.noreply.github.com>
2024-06-13 01:14:00 +0000

    Conversion of data types

---
+++ day_01/conversionOperations.js
@@ -0,0 +1,32 @@
+let score = 10
+console.log (typeof score)
+
+
+/* 
+suppose you get a number in string
+You have to convert string into a number
+for the you have to use Number
+
+e.g.,
+*/
+let newScore = "20"
+let valueInNumber = Number (newScore)
+console.log (newScore)
+
+// JUST FOR FUN
+
+let revisedScore = null
+let valueInNumberRevised = Number(revisedScore)
+console.log( typeof valueInNumberRevised)
+console.log(valueInNumberRevised )
+
+/*
+"33" => 33
+
+*/
+
+
+// FUN FACT
+console.log(typeof NaN)
+
+// number
